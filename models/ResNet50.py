e95a9e29de7883608fbe9864fe5ae5c062d2e484
Kostas <koxafis@gmail.com>
2025-01-14 13:42:08 +0200

    Updated ResNet50 results

--- models/ResNet50.py
+++ models/ResNet50.py
@@ -1,10 +1,10 @@
-from torchvision import models
+from torchvision.models import resnet50, ResNet50_Weights
 from models.custom_cnn import CustomCNN
 
 class Resnet50Pretrained(CustomCNN):
     def __init__(self, args: dict[str, any], class_count: int):
-        super(Resnet50Pretrained, self).__init__(args, class_count, None)
-        self.net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        super(Resnet50Pretrained, self).__init__(args, class_count)
+        self.net = resnet50(weights=ResNet50_Weights.DEFAULT)
 
     def to(self, device):
         self.net.to(device)
@@ -15,8 +15,8 @@ class Resnet50Pretrained(CustomCNN):
 
 class Resnet50(CustomCNN):
     def __init__(self, args: dict[str, any], class_count: int):
-        super(Resnet50, self).__init__(args, class_count, None)
-        self.net = models.resnet50(weights=False)
+        super(Resnet50, self).__init__(args, class_count)
+        self.net = resnet50(pretrained=False)
 
     def to(self, device):
         self.net.to(device)
